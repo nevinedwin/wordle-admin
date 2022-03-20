26bada63bf1d58587be3c0b636596b76d8703f27
Rohith-SK <rohith.sk@inapp.com>
2022-03-20 16:26:11 +0530

    API Integrated

--- src/Components/main-page/main-page.js
+++ src/Components/main-page/main-page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'
 import { Container, Row, Col, Table } from 'react-bootstrap'
@@ -7,18 +7,32 @@ import { Button } from 'react-bootstrap'
 import { ToastContainer, toast } from 'react-toastify';
 import data from '../../data'
 import './main-page.css'
+import { allPlayerDetails, getWord, playerDetailsByDay, setWord } from '../../services/apiServices';
 
 
 function MainPage() {
-    const [currentWordStatus, setCurrentWordStatus] = useState(false)
+    const [currentWordStatus, setCurrentWordStatus] = useState(true)
     const [playerDetailsStatus, setPlayerDetailsStatus] = useState(false)
     const [playerDetails, setPlayerDetails] = useState(false)
-    const [count, setCount] = useState(1)
+    const [dateArray, setDateArray] = useState([])
+    const [word, setWord] = useState('')
+    const [userDetails, setUserDetails] = useState({})
     const [wordleData, setWordleData] = useState({
-        word: '',
-        day: ''
+        word: ''
     })
     const navigate = useNavigate()
+
+    useEffect(() => {
+        allPlayerDetails().then(res => {
+            const details = res.data.messages
+            details.forEach(item => {
+
+                if (!dateArray.includes(item.date)) {
+                    setDateArray(prev => [...prev, item.date])
+                }
+            })
+        })
+    }, [])
 
     const inputHandler = (event) => {
         const { name, value } = event.target
@@ -33,27 +47,62 @@ function MainPage() {
 
     const formHandler = (event) => {
         event.preventDefault()
-        if (wordleData.word === '' || wordleData.day === '') {
+        if (wordleData.word === '') {
             toast.warn('Please enter the hidden word')
         }
         else {
-            setCurrentWordStatus(false)
-            console.log(wordleData)
+            if (wordleData.word.length === 5) {
+                console.log(wordleData.word)
+                setWord({ word: wordleData.word })
+                // .then(res => {
+                //     if (res.status == 200) {
+                //         toast.success('Hidden Word Updated Successfully')
+                //         setWordleData({
+                //             word: ''
+                //         })
+                //     }
+                //     else {
+                //         toast.warn('Error')
+                //     }
+                // })
+
+            }
+            else {
+                toast.error('Enter a 5 letter word')
+                setWordleData({
+                    word: '',
+                })
+            }
+
         }
     }
+
+
     const statusHandler = () => {
         setCurrentWordStatus(false)
         setPlayerDetailsStatus(true)
     }
 
-    const detailsHandler = (event) => {
+    const detailsHandler = (date) => {
         setPlayerDetails(true)
-        console.log(event.target.value)
+        getWord(date).then(res=>{
+            setWord(res.data.result.word)
+        })
+        playerDetailsByDay(date).then(res => {
+            if (res.status === 200) {
+                setUserDetails(res.data.messages)
+            }
+            else {
+                toast.error('Error in API fetch user details')
+            }
+        })
     }
 
-    const closeHandler = () =>{
+    const closeHandler = () => {
+        setCurrentWordStatus(true)
         setPlayerDetailsStatus(false)
         setPlayerDetails(false)
+
     }
 
 
@@ -79,10 +128,7 @@ function MainPage() {
                             <form onSubmit={formHandler}>
                                 <Form.Label className='main-page-form-label'>Enter Hidden Word</Form.Label>
                                 <Form.Control className='main-page-form-input' type='text' name='word' value={wordleData.word} placeholder='Enter the hidden word' onChange={inputHandler}></Form.Control>
-                                <Form.Label className='main-page-form-label'>Enter the day</Form.Label>
-                                <Form.Control className='main-page-form-input' type='text' name='day' value={wordleData.day} placeholder='Enter the day' onChange={inputHandler}></Form.Control>
                                 <Button className='done-button' type='submit' size='lg' variant='success'>Done</Button>
-                                <Button className='close-button' type='submit' size='lg' variant='success' onClick={() => setCurrentWordStatus(false)}>Close</Button>
                             </form>
                         </Col>
                     </Row>
@@ -90,36 +136,43 @@ function MainPage() {
             }
             {
                 playerDetailsStatus ? <div className='button-div'>
-                    <Button className='close-button' type='submit' size='lg' variant='success' value='1' onClick={detailsHandler}>Day 1</Button>
-                    <Button className='close-button' type='submit' size='lg' variant='success' value='2'  onClick={detailsHandler}>Day 2</Button>
-                    <Button className='close-button' type='submit' size='lg' variant='success' value='3'  onClick={detailsHandler}>Day 3</Button>
-                    <Button className='close-button' type='submit' size='lg' variant='success' value='4'  onClick={detailsHandler}>Day 4</Button>
-                    <Button className='close-button' type='submit' size='lg' variant='success' value='5'  onClick={detailsHandler}>Day 5</Button>
+                    {
+                        dateArray.map((today, index) => {
+
+                            return (
+                                <Button key={today} className='close-button' type='submit' size='lg' variant='success' onClick={() => detailsHandler(today)}>Day {index + 1} - {today.slice(8, 11)}</Button>
+                            )
+                        })
+                    }
                     <Button className='close-button' type='submit' size='lg' variant='success' onClick={closeHandler}>Close</Button>
+
                 </div> : null
             }
             {
                 playerDetails ? <div>
+                    <h2>Word : {word}</h2>
                     <Table className='main-page-table' striped bordered hover>
                         <thead>
                             <tr>
                                 <th>Email</th>
-                                <th>Date</th>
-                                <th>Time</th>
-                                <th>Game Status</th>
                                 <th>Attempt</th>
+                                <th>Time</th>
+                                <th>Score</th>
+                                <th>Game Status</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                Object.keys(data).map(function (key) {
-                                    return <tr>
-                                        <td key={data[key].email}>{data[key].email}</td>
-                                        <td key={data[key].email}>{data[key].day}</td>
-                                        <td key={data[key].email}>{data[key].time}</td>
-                                        <td key={data[key].email}>{data[key].gameStatus}</td>
-                                        <td key={data[key].email}>{data[key].attempt}</td>
-                                    </tr>
+                                userDetails.length > 0 && userDetails.map(item => {
+                                    return (
+                                        <tr key={item.email}>
+                                            <td>{item.email}</td>
+                                            <td>{item.attempt}</td>
+                                            <td>{item.time}</td>
+                                            <td>{item.score}</td>
+                                            <td>{item.gameStatus}</td>
+                                        </tr>
+                                    )
                                 })
                             }
                         </tbody>
