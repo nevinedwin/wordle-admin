60baf31b976bd5ef623f24f911ec25e3e8377478
Nevin Edwin <nevin.e@inapp.com>
2022-03-23 10:18:02 +0530

    updated api

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
-export const setWord = async(body)=>{
+export const setWord = async (body) => {
     return axios.post('http://15.206.92.254:3000/admin/setWord', body)
 }
 
-export const playerDetailsByDay = async (day)=>{
-    return axios.get(`http://15.206.92.254:3000/admin/userDetails?day=${day}`)
+export const playerDetailsByDay = async (day) => {
+    return axios.get(`http://15.206.92.254:3000/admin/userDetails?date=${day}`)
 }
 
-export const allPlayerDetails = async ()=>{
+export const allPlayerDetails = async () => {
     return axios.get(`http://15.206.92.254:3000/admin/userDetails`)
 }
 
-export const getWord = async (day)=>{
+export const getWord = async (day) => {
     return axios.get(`http://15.206.92.254:3000/users/getWord?date=${day}`)
 }
 
