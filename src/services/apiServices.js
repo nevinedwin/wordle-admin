9d88395f1fe6857e6aeafbcfbcff20a96cf49463
Rohith-SK <rohith.sk@inapp.com>
2022-04-08 12:39:44 +0530

    updated for aala

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 export const setWord = async (body) => {
-    return axios.post('http://15.206.92.254:3000/admin/setWord', body)
+    return axios.post('http://13.233.253.252:3000/admin/setWord', body)
 }
 
 export const playerDetailsByDay = async (day) => {
-    return axios.get(`http://15.206.92.254:3000/admin/userDetails?date=${day}`)
+    return axios.get(`http://13.233.253.252:3000/admin/userDetails?date=${day}`)
 }
 
 export const allPlayerDetails = async () => {
-    return axios.get(`http://15.206.92.254:3000/admin/userDetails`)
+    return axios.get(`http://13.233.253.252:3000/admin/userDetails`)
 }
 
 export const getWord = async (day) => {
-    return axios.get(`http://15.206.92.254:3000/users/getWord?date=${day}`)
+    return axios.get(`http://13.233.253.252:3000/users/getWord?date=${day}`)
 }
 
 
